a64c78237cbb8dd71d59249b600fd64c4b882955
geunhayou66 <64211521+geunhayou66@users.noreply.github.com>
2020-11-17 05:12:51 +0900

    Add files via upload

--- deobfuscator/deobfuscator.py
+++ deobfuscator/deobfuscator.py
@@ -1,18 +1,21 @@
 from profile import profile
 import opaque_id
 import opaque_location
+import os
+import subprocess
 from classes import dexfile
 dex = "../.apk/classes.dex"
 
 def main(dex):
-    #ret = opaque_id("/mnt/test/01_ControlFlow/01_High/sanity_cf.apk")
+	#ret = opaque_id("/mnt/test/01_ControlFlow/01_High/sanity_cf.apk")
     ret = opaque_id.opaque_id(dex)
+        
     if not ret :
         return
     profile(".stdout")
     ret = opaque_location.opaque_locations(dex)
     print("location : " + str(ret))  
-    if not ret :
-        return
+#    if not ret :
+#        return
     dexfile(dex)
 #main(dex)
--- deobfuscator/opaque_location.py
+++ deobfuscator/opaque_location.py
@@ -12,6 +12,7 @@ def opaque_location(apk_info):
     global stderr
     child = subprocess.Popen([os.getenv('ANDROID_HOST_OUT')+'/bin/OLocation'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     output = child.communicate(apk_info)
+    print('output:   ' + str(output))
     child_stdout, child_stderr = output[0], output[1]
     if len(output[1]) :
         stderr.write(output[1])
@@ -32,9 +33,11 @@ def opaque_locations(apk):
     stdout = open(os.environ['ROOT']+"/.stdout2", 'w')
     stderr = open(os.environ['ROOT']+"/.stderr2", 'w')
     lines = profile.readlines()
+    print("lines:    ::::   " + str(lines))
     
     for line in lines : 
         if not opaque_location(apk + " " + line) :
+            stdout.close()
             return False
-
+    stdout.close()
     return True
