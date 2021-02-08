5e74c754d1f47d0783170b7bb02d33df20bab7ea
Gyoonus <38876407+Gyoonus@users.noreply.github.com>
2021-02-08 14:36:30 +0900

    Update deobfuscator.py

--- deobfuscator/deobfuscator.py
+++ deobfuscator/deobfuscator.py
@@ -7,15 +7,17 @@ from classes import dexfile
 dex = "../.apk/classes.dex"
 
 def main(dex):
-	#ret = opaque_id("/mnt/test/01_ControlFlow/01_High/sanity_cf.apk")
+    '''
+    Entry Point of deobfuscator
+    [dex] : APK's path
+    '''
     ret = opaque_id.opaque_id(dex)
-        
+
     if not ret :
         return
+
     profile(".stdout")
     ret = opaque_location.opaque_locations(dex)
     print("location : " + str(ret))  
-#    if not ret :
-#        return
+
     dexfile(dex)
-#main(dex)
